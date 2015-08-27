0ad3679de919b201433d33eac5c821f0a7b591cf
mks2906 <manoj.singh@clear2pay.com>
2015-08-27 21:18:28 +0530

    code base for cms

---
+++ copywar.bat
@@ -0,0 +1,3 @@
+del /Q C:\Softwares\apache-tomcat-7.0.47\webapps\*.*
+rmdir /Q /S C:\Softwares\apache-tomcat-7.0.47\webapps\adminportal
+copy C:\Renovite\Project\CW\codebase\Issuer\dist\Issuer.war C:\Softwares\apache-tomcat-7.0.47\webapps\
